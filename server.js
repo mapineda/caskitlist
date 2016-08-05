b277d24668678c26a1efe2e884cd88331bcb63e0
mapineda <mapineda1222@gmail.com>
2016-08-05 17:18:20 -0500

    redo app architecture

--- server.js
+++ server.js
@@ -1,89 +1,61 @@
-var express = require("express");
-var path = require("path");
-var bodyParser = require("body-parser");
-var mongodb = require("mongodb");
-var ObjectID = mongod.ObjectID;
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-var NOTES_COLLECTION = "notes";
+var routes = require('./routes/index');
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.DB_CL_NOTES);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Create a db variable outside of the db connection callback to reuse the connection pool in app
-var db;
+app.use('/', routes);
 
-//Connect to database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, database) {
-  if(err) {
-    console.log(err);
-    process.exit(1);
-  }
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-  //save database boject from the callback for reuse.
-  db = database;
-  console.log("Database connection ready, commence world domination.")
+// error handlers
 
-  //Initialize the app.
-  var server = app.listen(process.env.PORT || 3500, function () {
-    var port = server.address().port;
-    console.log("Caskit api now alive on port", port);
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
   });
 });
 
-//NOTE API ROUTES BELOW
 
-// error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("Error: " + reason);
-  res.status(code || 500).json({"error": message});
-}
-
-/* "/notes"
- * GET: finds all notes
- * POST: creates a new note
- */
-
-// GET
- app.get("/notes", function(req, res) {
-
- });
-
-// Post
- app.post("/notes", function(req, res) {
-   var newNote = req.body;
-   newNote.createDate = new Date();
-
-   if(!(req.body.title || req.body.value)) {
-     handleError(res, "Invalid user input", "Must provide a title or value.", 400);
-   }
-
-   db.collection(NOTES_COLLECTION).insertOne(newNote, function(err, doc) {
-     if(err) {
-       handleError(res, err.message, "Failed to create new note");
-     } else {
-       res.status(201).json(doc.ops[0]);
-     }
-   });
- });
-
- /*   "/notes/:id"
-  *   GET find notes by id
-  *   PUT: update notes by id
-  *   DELETE: deletes notes by id
-  */
-
-// GET NOTES/:ID
-app.get("/notes/:id", function(req, res) {
-
-});
-
-//PUT NOTES/:ID
-app.put("/notes/:id", function(req, res) {
-
-});
-
-//DELETE/:ID
-app.delete("notes/:id", function(req, res) {
-
-});
+module.exports = app;
